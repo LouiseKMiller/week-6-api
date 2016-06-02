8b46a098c700d4deb4656fabecdf2c95cc3208e4
Louise Miller <louise.austex@gmail.com>
2016-06-02 10:51:53 -0500

    added reset.css, add starter buttons, create and show functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,5 +3,46 @@
 
 window.onload = function(){
 
+//======================================================
+// OBJECT THAT HOLDS ALL GIPH INFORMATION
+
+var allActions = [
+	{action: "pointing",
+	 status: "still"},
+	 {action: "laughing",
+	 status: "still"},
+	 {action: "sneezing",
+	 status: "still"},
+	 {action: "crying",
+	 status: "still"},
+	 {action: "driving",
+	 status: "still"},
+	];
+
+//=================================
+//  THIS CREATES A BUTTON FOR EACH ACTION IN THE allActions OBJECT
+
+function showButtons() {
+	$('#buttonDiv').empty();
+	for (var i =0; i<allActions.length; i++) {
+		$newButton = $('<button>');
+		$newButton.addClass("actionButton");
+		$newButton.text(allActions[i].action);
+		$('#buttonDiv').append($newButton);
+	}
+};
+
+//======================================
+//  EVENT HANDLER FOR USER SUBMISSION OF NEW ACTION
+
+$('#inputButton').on("click", function(event){
+	allActions.push(
+		{action:($('#userInput').val()),
+		status: "still"});
+	showButtons();
+	event.preventDefault(); // prevent default action associated with form submit
+});
+
+showButtons();
 
 } //end of window.onload function
